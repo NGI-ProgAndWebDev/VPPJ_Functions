32065bf21258aa231c635ebc3be131bf7074ced7
TysonSingh123 <137153676+TysonSingh123@users.noreply.github.com>
2023-06-20 10:33:47 +0200

    Add files via upload

--- Functions/DocComments.java
+++ Functions/DocComments.java
@@ -3,27 +3,37 @@ public class DocComments {
     public static void main(String[] args) {
         
     }
-    /*
-     * Function name: greet
-     * 
-     * Inside Function - prints hi
-     */
-    public static void greet() {
-       System.out.println("Hello"); 
-    }
 
     /*
-     * Fuction name: printText
+     * Function Name:greet 
      * 
+     * Inside Function:prints hi 
+     */
+    public static void greet(){
+        System.out.println("Hello")
+    }
+    
+    /**
+     * Function Name: printText
      * @param name
      * @param age
      */
-public static void printText(String name, String age) {
-    System.out.println("Hi, I am" + name + "and I am" + age + "years old");
-    
-}
-public static double calculateArea(double length , double width) {
-   double area =  length * width;
-   return area;
-}
-}
+    public static void printText(String name, String age){
+        System.out.println("Hi, I am" + name + "and i am" + age + "years old");
+    }
+    /**
+     * Fuction Name: calculatetree
+     * @param length
+     * @param width
+     * @return
+     */
+
+    @Override
+    public String toString() {
+        return "DocComments []";
+    }
+      
+        
+    }
+
+
